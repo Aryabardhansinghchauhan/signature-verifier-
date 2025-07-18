3294dbc6ac7795cdde37b70742f61aa6f15a191b
Aryabardhansinghchauhan <aryabardhansinghchauhan@gmail.com>
2025-07-18 10:09:12 +0530

    Delete wer.py

--- wer.py
+++
@@ -1,5 +0,0 @@
-from transformers import VisionEncoderDecoderModel  # ✅ Import the model
-
-model_path = r"C:\Users\aryac\Downloads\TrOCR-Signature-Forgery-master\TrOCR-Signature-Forgery-master"
-
-model = VisionEncoderDecoderModel.from_pretrained(model_path)  # ✅ No more NameError
